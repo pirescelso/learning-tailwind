65f890f320813c77c41313426d7475bc1bf01fa6
pirescelso <83796744+pirescelso@users.noreply.github.com>
2023-05-01 19:01:25 +0000

    initial setup

---
+++ src/app/example/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+export default function Example() {
+  return (
+    <div className="max-3 h-screen flex flex-col sm:flex-row items-center justify-center">
+<button type="button" className="
+  text-white
+  bg-blue-500
+  hover:bg-blue-700
+  py-2 
+  px-4 
+  m-0.5
+  text-sm
+  rounded-md
+">More Info</button>
+
+<button type="button" className="
+  text-white
+  bg-red-500
+  hover:bg-red-700
+  py-2 
+  px-4 
+  m-0.5
+  text-sm
+  rounded-md
+">Docs</button>
+
+<button className="btn">other</button>
+
+<button className="btn">other1</button>
+
+</div>
+  )
+}
